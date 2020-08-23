4da00a11c7390bb40032dfa8cbf1f0cb05bf77ed
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-23 23:00:52 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+# define code here
+class Dog 
+ def bark
+   puts "Woof!"
+  Dog.bark 
+   def sit
+     puts "The Dog is sitting".
+  Dog.sit   
+end
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+
+class Person
+  person instance = Person.new
+  def talk
+    puts "Hello World!"
+    Person.talk
+  def walk
+      puts "The Person is walking".
+      Person.walk
+    end
+  end
+end  
+
+  
